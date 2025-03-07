035458cb85c6d65de14f633909e7188dc9dc4f4f
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-07 20:21:52 +0800

    Digit Groups in order.
    Data-attribute added and digit groups in order.
    0 takes twice the space

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const calculator = {
+  initializeMainGroupItems() {
+    const mainGroup = document.querySelector("#calculator-main-group");
+
+    for (let i = 0; i < 10; i++) {
+      const digit = document.createElement("button");
+      digit.textContent = i;
+      digit.dataset.digit = i;
+      mainGroup.appendChild(digit);
+      if (i === 0) {
+        const decimal = document.createElement("button");
+        decimal.textContent = ".";
+        decimal.setAttribute("id", "calculator-decimal");
+        mainGroup.appendChild(decimal);
+      }
+    }
+  },
+};
+calculator.initializeMainGroupItems();
