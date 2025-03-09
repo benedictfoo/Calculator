e682ea3104caed8c546f0c97bb10611ea2a8e90e
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-03-09 21:00:56 +0800

    Colors for buttons

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ const calculator = {
     const leftGroup = document.querySelector("#calculator-left-group");
     const btnDigits = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "."];
     btnDigits.forEach((digit) => {
-      const button = document.createElement("button");
       if (digit === ".") {
         const decimal = document.createElement("button");
         decimal.textContent = ".";
         decimal.setAttribute("id", "calculator-decimal");
         leftGroup.appendChild(decimal);
       } else {
+        const button = document.createElement("button");
+        button.setAttribute("class", "calculator-digit");
         button.textContent = digit;
         button.dataset.button = digit;
         leftGroup.appendChild(button);
